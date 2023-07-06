25991eab343008e196ed501749a212cb02f09ad5
hauhuynh66 <hauhuynh66@gmail.com>
2023-07-06 18:42:09 +0700

    update

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { Prop, Schema, raw } from "@nestjs/mongoose"
+import { HydratedDocument } from "mongoose"
+
+export type UserDocument = HydratedDocument<User>
+
+@Schema()
+export class User {
+    @Prop({required: true, type: String})
+    firstname: String
+    
+    @Prop({required: true, type: String})
+    lastname: String
+
+    @Prop({required: true, type: String})
+    username: String
+
+    @Prop({type: Date})
+    birthDate: Date
+
+    @Prop({type: String})
+    password: String
+
+    @Prop({required: true, type: String})
+    email: String
+
+    @Prop(raw({
+        firstname: {type : String},
+        lastname: {type: String},
+        username: {type: String},
+        email: {type: String}
+    }))
+    raw: Record<string, any>
+}
